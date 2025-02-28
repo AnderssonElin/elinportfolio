426b1a725902b13b0e1e931ab2ce19b35d303031
AnderssonElin <von.elin@gmail.com>
2025-02-28 20:34:56 +0100

    Build vite build file and changing outdir to build

--- vite.config.ts
+++ vite.config.ts
@@ -19,4 +19,7 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: 'build/client' // Lägg till denna rad för att ställa in utgångskatalogen
+  }
 }));
