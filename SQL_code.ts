dcffa8acfb056fb2b5b1f9048b95445a73224c64
AnderssonElin <von.elin@gmail.com>
2025-03-02 17:34:41 +0100

    changed sql image

---
+++ SQL_code.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+    allowedHosts: [
+      "10f0d912-5d7f-494c-af9e-f27feebeb1ec.lovableproject.com"
+    ]
+  },
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  // Needed for deploying to netlify
+  // build: {
+  //   outDir: 'build/client'
+  // }
+}));
